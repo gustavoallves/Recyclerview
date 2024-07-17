7f6fcbfdb139d844ae5a50446175dcfea01491fe
Gustavo <gu.alvesslv@gmail.com>
2024-07-17 00:17:26 -0300

    Melhorando experiência do usuário

--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.devspace.recyclerview
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,64 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val rvList = findViewById<RecyclerView>(R.id.rv_list)
+        val ivList = findViewById<ImageView>(R.id.iv_list)
+        val ivGrid = findViewById<ImageView>(R.id.iv_grid)
+        val adapter = ContactListAdapter()
+
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+        adapter.submitList(contacts)
+
+        ivGrid.setOnClickListener {
+            rvList.layoutManager = GridLayoutManager(this, 2)
+        }
+
+        ivList.setOnClickListener {
+            rvList.layoutManager = LinearLayoutManager(this)
+        }
+
+        adapter.setOnClickListener { contact ->
+            val intent = Intent(this, ContactDetailActivity::class.java)
+            intent.putExtra("name", contact.name)
+            intent.putExtra("phone", contact.phone)
+            intent.putExtra("icon", contact.icon)
+            startActivity(intent)
+        }
+
     }
 }
+
+private val contacts = listOf(
+    Contact(
+        name = "Ana",
+        phone = "(55)21 239842348",
+        icon = R.drawable.sample6
+    ),
+    Contact(
+        name = "Miguel",
+        phone = "(55)21 738264836",
+        icon = R.drawable.sample2
+    ),
+    Contact(
+        name = "Beatriz",
+        phone = "(55)21 829374654",
+        icon = R.drawable.sample1
+    ),
+    Contact(
+        name = "Júlia",
+        phone = "(55)21 625382632",
+        icon = R.drawable.sample11
+    ),
+    Contact(
+        name = "Pedro",
+        phone = "(55)21 263865456",
+        icon = R.drawable.sample13
+    ),
+    Contact(
+        name = "Lucas",
+        phone = "(55)21 726538768",
+        icon = R.drawable.sample9
+    ),
+)
